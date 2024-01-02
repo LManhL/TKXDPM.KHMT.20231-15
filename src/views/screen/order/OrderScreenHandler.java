237aa9e902d0724fad1cacdd3b7f463104e1e01d
ManhPineapple <aktaovipma@gmail,com>
2024-01-02 09:23:03 +0700

    Home to ViewCart

--- src/views/screen/order/OrderScreenHandler.java
+++ src/views/screen/order/OrderScreenHandler.java
@@ -4,12 +4,11 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.swing.text.html.ListView;
-
 import controller.ViewOrderController;
 import entity.order.Order;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import views.screen.BaseScreenHandler;
@@ -26,20 +25,16 @@ public class OrderScreenHandler extends BaseScreenHandler {
 
     public OrderScreenHandler(Stage stage, String screenPath) throws IOException {
 		super(stage, screenPath);
-        this.controller = new ViewOrderController();
     }
 
     @FXML
-    public void initialize() {
+    public void initialize() throws SQLException {
+        this.controller = new ViewOrderController();
         deleteOrder.setOnAction(e -> {
             Order selectedOrder = orderList.getSelectionModel().getSelectedItem();
             if (selectedOrder != null) {
-                try {
-                    controller.deleteOrder(selectedOrder.getPhone());
-                    orderList.getItems().remove(selectedOrder);
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
+                controller.deleteOrder(selectedOrder.getPhone());
+				orderList.getItems().remove(selectedOrder);
             }
         });
     }
@@ -48,7 +43,7 @@ public class OrderScreenHandler extends BaseScreenHandler {
     public void show() {
         super.show();
         VBox vbox = new VBox();
-        List<Order> orders;
+        List<Order> orders = null;
 		try {
 			orders = controller.getAllOrders();
 		} catch (SQLException e) {
@@ -57,11 +52,7 @@ public class OrderScreenHandler extends BaseScreenHandler {
         for (Order order : orders) {
             Button button = new Button("Order : " + order.getId());
             button.setOnAction(e -> {
-                try {
-                    controller.deleteOrder(order.getPhone());
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
+                controller.deleteOrder(order.getPhone());
             });
             vbox.getChildren().add(button);
         }
