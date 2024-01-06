cad692b1e33e6a5b284eabe45770aca8efe1e325
ducminhsw <119603933+ducminhsw@users.noreply.github.com>
2024-01-06 21:38:57 +0700

    commit update image

--- src/entity/media/CD.java
+++ src/entity/media/CD.java
@@ -113,4 +113,18 @@ public class CD extends Media {
         return null;
     }
 
+    public String createCDQuery(String artist, String recordLabel, String musicType, String releaseDate) throws SQLException {
+        StringBuilder queryValues = new StringBuilder();
+        queryValues.append("(")
+                .append("placeForId").append(", ")
+                .append("'").append(artist).append("'").append(", ")
+                .append("'").append(recordLabel).append("'").append(", ")
+                .append("'").append(musicType).append("'").append(", ")
+                .append("'").append(releaseDate).append("'").append(")");
+        String sql = "INSERT INTO CD "
+                + "(id, artist, recordLabel, musicType, releasedDate)"
+                + " VALUES "
+                + queryValues.toString() + ";";
+        return sql;
+    }
 }
